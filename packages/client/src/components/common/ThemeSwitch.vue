140e1571a67598cf95e12ef03d61cd565d471bbc
adibarra <93070681+adibarra@users.noreply.github.com>
2024-11-05 04:37:19 +0000

    add theme switch

---
+++ packages/client/src/components/common/ThemeSwitch.vue
@@ -0,0 +1,33 @@
+<!--
+  @author: adibarra (Alec Ibarra)
+  @description: This component is used to switch the theme of the application.
+-->
+
+<script setup lang="ts">
+import {
+  Moon as DarkIcon,
+  Sun as LightIcon,
+} from '@vicons/carbon'
+import { useMessage } from 'naive-ui'
+
+const message = useMessage()
+</script>
+
+<template>
+  <n-tooltip>
+    <template #trigger>
+      <n-button
+        text @click="() => {
+          message.info('Theme toggled')
+          toggleTheme()
+        }"
+      >
+        <n-icon size="22">
+          <DarkIcon v-if="isDark" />
+          <LightIcon v-else />
+        </n-icon>
+      </n-button>
+    </template>
+    Toggle Theme
+  </n-tooltip>
+</template>
