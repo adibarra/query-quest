5999fd2b01cc39d91aeeac63dfc1a20b90db1f28
adibarra <93070681+adibarra@users.noreply.github.com>
2024-11-05 04:55:07 +0000

    standardize header with navbar

--- packages/client/src/components/common/Header.vue
+++ packages/client/src/components/common/Header.vue
@@ -4,43 +4,22 @@
 -->
 
 <template>
-  <header relative sticky top-0 z-999 flex b-b-1 b-b--c-tertiary bg--c-primary px-3 py-2 sm:py-3>
-    <div absolute left-0 right-0 my-auto>
-      <slot />
+  <header h-48px w-full flex b-b-1 b-b--c-tertiary bg--c-primary>
+    <div flex grow items-center py-1>
+      <div hidden h-full w-55 items-center justify-center md:flex>
+        <Logo />
+      </div>
+      <div grow />
+      <nav my-auto mr-5 flex gap-5 sm:text-xl>
+        <RouterLink
+          to="/"
+          title="Home"
+          qq-icon-btn
+        >
+          <div i-carbon:home />
+        </RouterLink>
+        <ThemeSwitch />
+      </nav>
     </div>
-    <RouterLink
-      to="/"
-      title="Logo"
-      op-100 qq-icon-btn
-    >
-      <Logo />
-    </RouterLink>
-    <div grow />
-    <nav my-auto flex gap-x5 sm:text-xl>
-      <RouterLink
-        to="/"
-        title="Home"
-        qq-icon-btn
-      >
-        <div i-carbon:home />
-      </RouterLink>
-      <a
-        href="https://github.com/adibarra/query-quest"
-        target="_blank"
-        rel="noopener"
-        title="GitHub Repository"
-        qq-icon-btn
-      >
-        <div i-carbon:logo-github />
-      </a>
-      <button
-        title="Toggle Theme"
-        qq-icon-btn
-        @click="toggleTheme()"
-        @keydown.enter="toggleTheme()"
-      >
-        <div i="carbon-sun dark:carbon-moon" />
-      </button>
-    </nav>
   </header>
 </template>
