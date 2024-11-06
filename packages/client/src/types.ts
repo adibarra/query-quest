52f68246604592f95bf80a63edf77dfc55e9d5a5
adibarra <93070681+adibarra@users.noreply.github.com>
2024-11-06 22:31:12 +0000

    add api types

--- packages/client/src/types.ts
+++ packages/client/src/types.ts
@@ -7,3 +7,28 @@ import type { App } from 'vue'
 import type { Router } from 'vue-router'
 
 export type UserModule = (ctx: { app: App, router: Router }) => void
+
+export interface User {
+  id: string
+  username: string
+}
+
+export interface Question {
+  id: string
+  question: string
+  difficulty: number
+  options: string[]
+  tags: string[]
+}
+
+export interface Tag {
+  id: string
+  name: string
+  description: string
+}
+
+export interface Stats {
+  totalQuestions: number
+  totalTags: number
+  totalUsers: number
+}
