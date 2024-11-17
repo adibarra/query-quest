6f5d7c7923d1f21a70d77b80c2c6f2115f568fa1
adibarra <93070681+adibarra@users.noreply.github.com>
2024-11-17 08:06:00 +0000

    provide better message for missing auth token

--- packages/server/src/helpers/requireAuth.py
+++ packages/server/src/helpers/requireAuth.py
@@ -10,7 +10,7 @@ db = Database()
 
 
 async def requireAuth(
-    authorization: str = Header(...),
+    authorization: str = Header(None),
 ) -> SessionDict:
     """
     Validates the provided Authorization token and returns a dictionary containing the validated token owner and the token itself.
@@ -34,11 +34,17 @@ async def requireAuth(
         HTTPException: If the token is invalid or not found in the database (401 Unauthorized).
     """
 
+    if authorization is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Bad Request: Missing Authorization Header",
+        )
+
     parts = authorization.split(" ")
     if len(parts) != 2 or parts[0].lower() != "bearer":
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Bad Request: Missing or Malformed Authorization Header",
+            detail="Bad Request: Malformed Authorization Header",
         )
 
     session = db.get_session(token=parts[1])
