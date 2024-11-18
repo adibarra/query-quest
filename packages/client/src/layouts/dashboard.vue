d7c8633b4ab97afb52aa5289fbfdf58f20b20759
adibarra <93070681+adibarra@users.noreply.github.com>
2024-11-18 17:49:46 +0000

    add loading spinner to dashboard

--- packages/client/src/layouts/dashboard.vue
+++ packages/client/src/layouts/dashboard.vue
@@ -13,19 +13,24 @@ watch(() => state.isAuthenticated, (auth) => {
 </script>
 
 <template>
-  <PullToRefresh />
-  <n-layout position="absolute" h-full>
-    <NavBar />
-    <n-layout has-sider :style="{ height: isMobile ? 'calc(100% - 108px)' : 'calc(100% - 48px)' }">
-      <SideBar v-if="!isMobile" />
-      <n-layout-content bg--c-primary text--c-text>
-        <div min-h-full w-full flex>
-          <div w-full flex grow flex-col px-6 py-4>
-            <RouterView />
+  <div v-if="state.isAuthenticated">
+    <PullToRefresh />
+    <n-layout position="absolute" h-full>
+      <NavBar />
+      <n-layout has-sider :style="{ height: isMobile ? 'calc(100% - 108px)' : 'calc(100% - 48px)' }">
+        <SideBar v-if="!isMobile" />
+        <n-layout-content bg--c-primary text--c-text>
+          <div min-h-full w-full flex>
+            <div w-full flex grow flex-col px-6 py-4>
+              <RouterView />
+            </div>
           </div>
-        </div>
-      </n-layout-content>
+        </n-layout-content>
+      </n-layout>
+      <FooterBar v-if="isMobile" />
     </n-layout>
-    <FooterBar v-if="isMobile" />
-  </n-layout>
+  </div>
+  <div v-else w-full flex justify-center min-h-svh>
+    <n-spin size="large" />
+  </div>
 </template>
